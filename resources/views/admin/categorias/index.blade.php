fae9997b8f2f80ed063e2d00c0759a5201eace59
Ricardo Saucedo <richar.saucedo@gmail.com>
2021-03-15 14:43:02 -0300

    install lteadmin

--- resources/views/admin/categorias/index.blade.php
+++ resources/views/admin/categorias/index.blade.php
@@ -1,19 +1,14 @@
 @extends('adminlte::page')
 
-@section('title', 'Dashboard')
+@section('title', 'Evaluaciones completadas')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <h1>Evaluaciones completadas</h1><hr>
+
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
-@stop
+	
+	hola
 
-@section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
-@stop
-
-@section('js')
-    <script> console.log('Hi!'); </script>
 @stop
