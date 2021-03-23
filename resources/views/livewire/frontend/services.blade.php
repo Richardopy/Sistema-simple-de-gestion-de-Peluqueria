b1c80f09d4982fe9cc0c459578c9ad7ae8992a16
Ricardo Saucedo <richar.saucedo@gmail.com>
2021-03-23 14:55:57 -0300

    add componente marcas, proveedores y unidades de medida

--- resources/views/livewire/frontend/services.blade.php
+++ resources/views/livewire/frontend/services.blade.php
@@ -5,28 +5,27 @@
 				<h3 class="w3l_header">Nuestros <span>Servicios</span></h3>
 				<p>Texto Genérico - Texto Genérico - Texto Genérico - Texto Genérico - Texto Genérico - Texto Genérico - Texto Genérico - Texto Genérico - Texto Genérico - Texto Genérico - Texto Genérico - Texto Genérico - Texto Genérico</p>
 			</div>
-			
-			@foreach ($servicios as $value)
-			<?php
-				if (empty($contador)){
-				   $contador = 1;
-
-				}else{
-				   $contador++;
-				endif
-				}
-				?>
-				<div class="area-main">
+			<div class="area-main">
+				@php
+					$cont=1;
+				@endphp
+				@foreach ($servicios as $value)
 					<div class="col-md-6 area-inner">
-						<div class="area-img.$contador">
+						<div class="area-img" style="background: url(/images/servicios/{{ $value->foto }})no-repeat 0px 0px;">
 						</div>
-						<div class="area-right p.$contador">
+						<div class="area-right p{{ $cont }}">
 							<h5>{{$value->nombre }}</h5>
-							<p class="para-w3-agile">{{$value->description }}</p>
+							<p class="para-w3-agile"><?= $value->description ?></p>
 						</div>
 					</div>
-
-			@endforeach
+					@if ($cont%2==0)
+						</div>
+						<div class="area-main">
+					@endif
+					@php
+						$cont+=1;
+					@endphp
+				@endforeach
 			</div>
 		</div>
 	</div>
