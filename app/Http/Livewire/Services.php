b67b3696c2068a2faa6b8b446812f91e8d4ab3b6
Pablo Ortega <salomonpabloortega@gmail.com>
2021-03-23 14:27:35 -0300

    Principio de introducir servicios a frontend

--- app/Http/Livewire/Services.php
+++ app/Http/Livewire/Services.php
@@ -3,11 +3,14 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Servicio;
 
 class Services extends Component
 {
     public function render()
     {
-        return view('livewire.frontend.services');
+    	$servicios = Servicio::where('estado',1)->get();
+        return view('livewire.frontend.services',["servicios"=>$servicios]);
     }
+
 }
