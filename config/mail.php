d10b7cc4f79987ccb82f90026f46b8b37261ef67
Pablo Ortega <salomonpabloortega@gmail.com>
2021-03-30 10:58:00 -0400

    Fusión de Vista Contacto envia email

--- config/mail.php
+++ config/mail.php
@@ -36,20 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-<<<<<<< HEAD
             'host' => env('MAIL_HOST', 'smtp.gmail.com'),
             'port' => env('MAIL_PORT', 465),
             'encryption' => env('MAIL_ENCRYPTION'),
             'username' => env('MAIL_USERNAME','tech.circle.peluqueria@gmail.com'),
-            'password' => env('MAIL_PASSWORD','ptsmmbrpzevuxwcy
-'),
-=======
-            'host' => env('MAIL_HOST', 'stmp.gmail.com'),
-            'port' => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
->>>>>>> 50c9a35329d62794401cab29c219d4f15a0e7ad6
+            'password' => env('MAIL_PASSWORD','ptsmmbrpzevuxwcy'),
             'timeout' => null,
             'auth_mode' => null,
         ],
