a86ee1773735f55c6de6db36951de95c974475b6
Ricardo Saucedo <richar.saucedo@gmail.com>
2021-04-23 14:39:11 -0400

    catalogo

--- app/Http/Livewire/Verproductos.php
+++ app/Http/Livewire/Verproductos.php
@@ -13,43 +13,14 @@ use App\Models\Producto;
 class Verproductos extends Component{
 
     protected $paginationTheme = 'bootstrap';
-    
-    public $search='';
-
-	public $LeerMode = false;
 
 	public $usuario_id,$cabecera_id=0;
 
     public function render(){    
 
-        $empresa = Empresa::findorFail(1);
+        $cabecera=CabeceraPedido::where('usuario_id',Auth::user()->id)->paginate('20');
 
-        $producto=DB::table('cabecera_pedidos as ca')
-            ->join('users as u','ca.usuario_id','u.id')
-            ->select('ca.*','u.name','u.contacto')
-            ->get()
-            ->where('u.name','LIKE','%'.$this->search.'%');
-
-        $cabecera = DB::table('cabecera_pedidos as ca')
-            ->join('users as u','ca.usuario_id','u.id')
-            ->select('ca.*','u.name','u.contacto')
-            ->where('ca.id',$this->cabecera_id)->first();
-
-        $productos = DB::table('pedidos as cp')
-            ->join('productos as p','cp.producto_id','p.id')
-            ->select('cp.*','p.nombre','p.foto')
-            ->where('cp.cabecera_id',$this->cabecera_id)
-            ->get();
-
-        return view('livewire.verproductos',["producto"=>$producto,"cabecera"=>$cabecera,"productos"=>$productos,"empresa"=>$empresa,"productos"=>$productos]);
-    }
-
-    public function estado($estado){
-    	$this->pedidoestado=$estado;
-        $this->cabecera_id=0;
-        $this->LeerMode = false;
-        $this->msmstate = $estado;
-
+        return view('livewire.verproductos',["cabecera"=>$cabecera]);
     }
 
     public function leer($id){
